44ad370f33f87bace5c5b1ef4328e3eaad6d616a
simplescripter <simplescripter@gmail.com>
2019-02-06 20:36:01 -0700

    Changed 2016 size to Standard_D2_v2

---
+++ .Build.ps1
@@ -0,0 +1,6 @@
+﻿Remove-AzResourceGroup TemplateTest2 -Force
+Sleep 10
+New-AzResourceGroup TemplateTest2 -Location centralus
+Read-Host "Hit Enter to continue..."
+New-AzResourceGroupDeployment -Name Dep1 -ResourceGroupName TemplateTest2 `
+    -TemplateUri https://raw.githubusercontent.com/simplescripter/AzProject/master/Main.json -Verbose
